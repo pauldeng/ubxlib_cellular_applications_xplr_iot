6facd515172e6136918d8e4d94b70d7b102603e6
Paul Deng <paul.deng@gallagher.com>
2023-08-23 12:09:36 +1000

    Fixed timestamp in cellScan function

--- applications/tasks/cellScanTask.c
+++ applications/tasks/cellScanTask.c
@@ -103,12 +103,12 @@ static void doCellScan(void *pParams)
     char timestamp[TIMESTAMP_MAX_LENTH_BYTES];
     getTimeStamp(timestamp);
 
-    char format[] = "{"                 \
-            "\"Timestamp\":\"%s\", "    \
-            "\"CellSCan\":{"            \
-                "\"Name\":\"%s\", "     \
-                "\"ubxlibRAT\":\"%d\", "     \
-                "\"MCCMNC\":\"%s\"}"   \
+    char format[] = "{"                      \
+            "\"Timestamp\":%" PRId64 ","     \
+            "\"CellSCan\":{"                 \
+                "\"Name\":\"%s\","           \
+                "\"ubxlibRAT\":\"%d\","      \
+                "\"MCCMNC\":\"%s\"}"         \
         "}";
 
     writeLog("Scanning for networks...");
@@ -119,7 +119,7 @@ static void doCellScan(void *pParams)
             count = uCellNetScanGetNext(gDeviceHandle, internalBuffer, sizeof(internalBuffer), mccMnc, &rat)) {
 
         found++;
-        snprintf(payload, sizeof(payload), format, timestamp, internalBuffer, rat, mccMnc);
+        snprintf(payload, sizeof(payload), format, (unixNetworkTime + (uPortGetTickTimeMs() / 1000)), internalBuffer, rat, mccMnc);
         writeAlways(payload);
         sendMQTTMessage(topicName, payload, U_MQTT_QOS_AT_MOST_ONCE, false);
     }
